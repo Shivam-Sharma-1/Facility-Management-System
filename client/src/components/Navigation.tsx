dbdcc9d1ebe44771f39ec8fcd60b25038eb911a2
Shivam77607 <shivamsharma77607@gmail.com>
2023-10-28 21:47:35 +0530

    style: update style

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,13 +1,47 @@
+import { useState } from 'react';
+
+import { Avatar, ListItemIcon } from '@mui/material';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import ApprovalIcon from '@mui/icons-material/Approval';
+import BookmarksIcon from '@mui/icons-material/Bookmarks';
+import EventIcon from '@mui/icons-material/Event';
+import SummarizeIcon from '@mui/icons-material/Summarize';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import { useState } from 'react';
-import { Avatar } from '@mui/material';
 
-function Navigation() {
+const navigationData: NavigationData[] = [
+  {
+    label: 'Admin',
+    icon: <AdminPanelSettingsIcon sx={{ width: "26px", height: "26px", color: "white" }} />,
+    key: 'admin'
+  },
+  {
+    label: 'Approval Status',
+    icon: <ApprovalIcon sx={{ width: "26px", height: "26px", color: "white" }} />,
+    key: 'approval'
+  },
+  {
+    label: 'Bookings',
+    icon: <BookmarksIcon sx={{ width: "26px", height: "26px", color: "white" }} />,
+    key: 'bookings'
+  },
+  {
+    label: 'Calender',
+    icon: <EventIcon sx={{ width: "26px", height: "26px", color: "white" }} />,
+    key: 'calender'
+  },
+  {
+    label: 'Report',
+    icon: <SummarizeIcon sx={{ width: "26px", height: "26px", color: "white" }} />,
+    key: 'report'
+  },
+];
+
+function Navigation(): JSX.Element {
   const [open, setOpen] = useState(false);
 
   const handleClick = () => {
@@ -16,30 +50,32 @@ function Navigation() {
 
   return (
     <div
-      className='w-[20%] h-full min-h-[100dvh] bg-primary text-white'
+      className='w-[20%] h-full min-h-[100dvh] bg-primary text-white pt-8'
     >
       <div className='w-full flex justify-center px-4 pt-4 gap-4'>
         <Avatar sx={{ width: "80px", height: "80px" }} src='https://www.w3schools.com/howto/img_avatar.png' alt="avatar-image" />
         <div className='flex flex-col justify-center gap-2'>
-          <h2>Shivam Sharma</h2>
+          <h3>Shivam Sharma</h3>
           <h4 className='font-normal'>ID: 6551</h4>
         </div>
       </div>
-      <div className='w-[92%] h-[1px] bg-slate-400 rounded-full m-auto mt-4'></div>
+      <div className='w-[92%] h-[1px] bg-slate-400 rounded-full mx-auto mt-6'></div>
       <List
         component="nav"
         aria-labelledby="nested-list-subheader"
       >
-        <ListItemButton>
-          <ListItemText primary="Admin" />
-        </ListItemButton>
-        <ListItemButton>
-          <ListItemText primary="Approval Status" />
-        </ListItemButton>
-        <ListItemButton onClick={handleClick}>
+        {navigationData.map(item => (
+          <ListItemButton key={item.key} className='flex gap-4'>
+            <ListItemIcon sx={{ minWidth: "0px" }}>
+              {item.icon}
+            </ListItemIcon>
+            <ListItemText primary={item.label} />
+          </ListItemButton>
+        ))}
+        {/* <ListItemButton onClick={handleClick}>
           <ListItemText primary="Inbox" />
           {open ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
+        </ListItemButton> */}
         <Collapse in={open} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
             <ListItemButton sx={{ pl: 4 }}>
@@ -47,15 +83,6 @@ function Navigation() {
             </ListItemButton>
           </List>
         </Collapse>
-        <ListItemButton>
-          <ListItemText primary="Bookings" />
-        </ListItemButton>
-        <ListItemButton>
-          <ListItemText primary="Calender" />
-        </ListItemButton>
-        <ListItemButton>
-          <ListItemText primary="Report" />
-        </ListItemButton>
       </List>
     </div>
   );
