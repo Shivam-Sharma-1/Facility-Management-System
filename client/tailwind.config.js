97deee386ddf04c3d3536eb62b0f0647bb5600f1
Shivam77607 <shivamsharma77607@gmail.com>
2023-10-28 17:02:03 +0530

    build: add tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#02447C",
+				bgPrimary: "#ffffff"
+			},
+			screens: {
+				xs: "320px",
+				sm: "640px",
+				md: "768px",
+				lg: "1024px",
+				xl: "1280px"
+			}
+		}
+	},
+	plugins: []
+};
