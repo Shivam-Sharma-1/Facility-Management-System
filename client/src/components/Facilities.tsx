06e07d84a4fe104097efcbb4ccabdd96bf55a690
Shivam77607 <shivamsharma77607@gmail.com>
2023-10-31 18:39:31 +0530

    feat: fetch data from database

--- client/src/components/Facilities.tsx
+++ client/src/components/Facilities.tsx
@@ -1,40 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 import FacilityCard from "./FacilityCard";
 
-const facilityData: FacilityData[] = [
-  {
-    name: "Gym",
-    icon: "/vite.svg",
-  },
-  {
-    name: "Food",
-    icon: "/vite.svg",
-  },
-  {
-    name: "Money",
-    icon: "/vite.svg",
-  },
-  {
-    name: "Trade",
-    icon: "/vite.svg",
-  },
-  {
-    name: "Hotel",
-    icon: "/vite.svg",
-  },
-  {
-    name: "Bro",
-    icon: "/vite.svg",
-  },
-];
-
 function Facilities(): JSX.Element {
+  const { data, isPending } = useQuery({
+    queryKey: ["facilities"],
+    queryFn: async () => {
+      const response = await axios.get<FacilityData[]>(
+        "http://localhost:3000/dashboard",
+        {
+          withCredentials: true,
+        }
+      );
+      return response.data;
+    },
+  });
+  console.log(data);
+
   return (
     <div className="w-[80%] h-full flex flex-col items-center justify-center text-black px-6 pt-12">
       <h1 className="uppercase">FACILITIES</h1>
       <div className="w-full flex justify-center items-center flex-wrap pt-4">
-        {facilityData.map((facility) => (
-          <FacilityCard key={facility.name} name={facility.name} icon={facility.icon} />
-        ))}
+        {!isPending &&
+          data?.map((facility: FacilityData) => (
+            <FacilityCard
+              key={facility.name}
+              name={facility.name}
+              icon={facility.icon}
+            />
+          ))}
       </div>
     </div>
   );
