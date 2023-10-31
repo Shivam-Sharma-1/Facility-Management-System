1b4706df7e84c5db2aa5835605b6a151435a96ee
Shivam77607 <shivamsharma77607@gmail.com>
2023-10-31 10:40:46 +0530

    feat: add logout option

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,50 +1,16 @@
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
 import { Avatar, ListItemIcon } from "@mui/material";
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import ApprovalIcon from "@mui/icons-material/Approval";
 import BookmarksIcon from "@mui/icons-material/Bookmarks";
 import EventIcon from "@mui/icons-material/Event";
 import SummarizeIcon from "@mui/icons-material/Summarize";
+import LogoutIcon from "@mui/icons-material/Logout";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-
-const navigationData: NavigationData[] = [
-  {
-    label: "Admin",
-    icon: (
-      <AdminPanelSettingsIcon
-        sx={{ width: "26px", height: "26px", color: "white" }}
-      />
-    ),
-    key: "admin",
-  },
-  {
-    label: "Approval Status",
-    icon: (
-      <ApprovalIcon sx={{ width: "26px", height: "26px", color: "white" }} />
-    ),
-    key: "approval",
-  },
-  {
-    label: "Bookings",
-    icon: (
-      <BookmarksIcon sx={{ width: "26px", height: "26px", color: "white" }} />
-    ),
-    key: "bookings",
-  },
-  {
-    label: "Calender",
-    icon: <EventIcon sx={{ width: "26px", height: "26px", color: "white" }} />,
-    key: "calender",
-  },
-  {
-    label: "Report",
-    icon: (
-      <SummarizeIcon sx={{ width: "26px", height: "26px", color: "white" }} />
-    ),
-    key: "report",
-  },
-];
 
 const Navigation = (): JSX.Element => {
   // const [open, setOpen] = useState(false);
@@ -52,6 +18,72 @@ const Navigation = (): JSX.Element => {
   // const handleClick = () => {
   //   setOpen(!open);
   // };
+
+  const navigationData: NavigationData[] = [
+    {
+      label: "Admin",
+      icon: (
+        <AdminPanelSettingsIcon
+          sx={{ width: "26px", height: "26px", color: "white" }}
+        />
+      ),
+      key: "admin",
+      onClick: () => console.log("admin"),
+    },
+    {
+      label: "Approval Status",
+      icon: (
+        <ApprovalIcon sx={{ width: "26px", height: "26px", color: "white" }} />
+      ),
+      key: "approval",
+      onClick: () => console.log("approval"),
+    },
+    {
+      label: "Bookings",
+      icon: (
+        <BookmarksIcon sx={{ width: "26px", height: "26px", color: "white" }} />
+      ),
+      key: "bookings",
+      onClick: () => console.log("bookings"),
+    },
+    {
+      label: "Calender",
+      icon: (
+        <EventIcon sx={{ width: "26px", height: "26px", color: "white" }} />
+      ),
+      key: "calender",
+      onClick: () => console.log("calender"),
+    },
+    {
+      label: "Report",
+      icon: (
+        <SummarizeIcon sx={{ width: "26px", height: "26px", color: "white" }} />
+      ),
+      key: "report",
+      onClick: () => console.log("report"),
+    },
+    {
+      label: "Logout",
+      icon: (
+        <LogoutIcon sx={{ width: "26px", height: "26px", color: "white" }} />
+      ),
+      key: "logout",
+      onClick: () => mutation.mutate(),
+    },
+  ];
+
+  const mutation = useMutation({
+    mutationFn: () =>
+      axios.post("http://localhost:3000/auth/logout", {
+        withCredentials: true,
+      }),
+    onSuccess: () => {
+      console.log("logout");
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
 
   return (
     <div className="w-[20%] h-full min-h-[100dvh] bg-primary text-white pt-8 overflow-y-hidden">
@@ -69,7 +101,11 @@ const Navigation = (): JSX.Element => {
       <div className="w-[92%] h-[1px] bg-slate-400 rounded-full mx-auto mt-6"></div>
       <List component="nav" aria-labelledby="nested-list-subheader">
         {navigationData.map((item) => (
-          <ListItemButton key={item.key} className="flex gap-4">
+          <ListItemButton
+            key={item.key}
+            className="flex gap-4"
+            onClick={() => item.onClick()}
+          >
             <ListItemIcon sx={{ minWidth: "0px" }}>{item.icon}</ListItemIcon>
             <ListItemText primary={item.label} />
           </ListItemButton>
