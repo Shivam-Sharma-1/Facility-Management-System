ea57aa681891b84a806e358690546a38ec0de645
Shivam77607 <shivamsharma77607@gmail.com>
2023-10-31 09:29:29 +0530

    feat: update calender

--- client/src/components/Calender.tsx
+++ client/src/components/Calender.tsx
@@ -1,22 +1,19 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import AddEventModal from "./AddEventModal";
-
-function handleEventClick(clickInfo: unknown): void {
-  console.log(clickInfo);
-}
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { EventSourceInput } from "@fullcalendar/core/index.js";
 
 const handleEventContent: FC<EventContentProps> = (eventInfo): JSX.Element => {
   return (
     <div
       className={`px-1 w-full rounded-sm text-white cursor-pointer`}
       style={{
-        backgroundColor: eventInfo.backgroundColor
-          ? eventInfo.backgroundColor
-          : "red",
+        backgroundColor: eventInfo.backgroundColor,
       }}
     >
       <i>{eventInfo.event.title}</i>
@@ -26,6 +23,30 @@ const handleEventContent: FC<EventContentProps> = (eventInfo): JSX.Element => {
 
 const Calendar: FC = (): JSX.Element => {
   const [isOpen, setIsOpen] = useState(false);
+  const [bookingsData, setBookingsData] = useState<BookingDataProps[]>();
+
+  const { data, isPending } = useQuery<BookingDataProps[]>({
+    queryKey: ["bookings"],
+    queryFn: async () => {
+      const response = await axios.get<BookingDataProps[]>(
+        "http://localhost:3000/facility/facility-1",
+        {
+          withCredentials: true,
+        }
+      );
+      return response.data;
+    },
+  });
+  console.log(data);
+
+  useEffect(() => {
+    if (!isPending) {
+      const newData = data?.map((booking) => {
+        return { ...booking, id: booking.id.toString() };
+      });
+      setBookingsData(newData || []);
+    }
+  }, [data, isPending]);
 
   return (
     <div className="w-[80%] h-full flex flex-col items-center justify-center text-black px-6 pt-12">
@@ -35,46 +56,13 @@ const Calendar: FC = (): JSX.Element => {
         <FullCalendar
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
-          events={[
-            {
-              title: "ok",
-              date: "2023-10-01",
-              color: "black",
-              start: "2023-10-01T10:30:00",
-              end: "2023-10-01T12:30:00",
-              requested_by: "user",
-              allDay: false,
-            },
-            {
-              title: "event 1",
-              color: "red",
-              start: "2023-10-01T10:00:00",
-              end: "2023-10-01T12:30:00",
-              allDay: false,
-            },
-            {
-              title: "event 2",
-              date: "2023-10-30",
-              color: "red",
-              allDay: false,
-            },
-            {
-              title: "test",
-              date: "2023-11-11T18:30:00.000Z",
-              color: "blue",
-              allDay: false,
-            },
-          ]}
+          events={bookingsData as EventSourceInput}
           headerToolbar={{
             left: "prev,next,today,addEventButton",
             center: "title",
             right: "dayGridMonth,timeGridWeek,timeGridDay",
           }}
-          // footerToolbar={{
-          //   center: "addEventButton",
-          // }}
-          eventClick={handleEventClick}
-          eventContent={handleEventContent}
+          eventContent={() => handleEventContent}
           customButtons={{
             addEventButton: {
               text: "Add event",
