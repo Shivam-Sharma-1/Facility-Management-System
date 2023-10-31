2d0cc370d760eabec78f758f693cc209deee818b
Shivam77607 <shivamsharma77607@gmail.com>
2023-10-31 18:40:07 +0530

    feat: fetch data from database

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
-import { Avatar, ListItemIcon } from "@mui/material";
+import { Avatar, Collapse, ListItemIcon } from "@mui/material";
+import { useAuth } from "./hooks/useAuth";
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import ApprovalIcon from "@mui/icons-material/Approval";
 import BookmarksIcon from "@mui/icons-material/Bookmarks";
@@ -11,14 +13,15 @@ import LogoutIcon from "@mui/icons-material/Logout";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-import { useAuth } from "./hooks/useAuth";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
 
 const Navigation = (): JSX.Element => {
-  // const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-  // const handleClick = () => {
-  //   setOpen(!open);
-  // };
+  const handleClick = () => {
+    setOpen(!open);
+  };
   const auth = useAuth();
 
   const navigationData: NavigationData[] = [
@@ -32,14 +35,14 @@ const Navigation = (): JSX.Element => {
       key: "admin",
       onClick: () => console.log("admin"),
     },
-    {
-      label: "Approval Status",
-      icon: (
-        <ApprovalIcon sx={{ width: "26px", height: "26px", color: "white" }} />
-      ),
-      key: "approval",
-      onClick: () => console.log("approval"),
-    },
+    // {
+    //   label: "Approval Status",
+    //   icon: (
+    //     <ApprovalIcon sx={{ width: "26px", height: "26px", color: "white" }} />
+    //   ),
+    //   key: "approval",
+    //   onClick: () => console.log("approval"),
+    // },
     {
       label: "Bookings",
       icon: (
@@ -95,37 +98,55 @@ const Navigation = (): JSX.Element => {
       <div className="w-full flex justify-center px-4 pt-4 gap-4 flex-wrap">
         <Avatar
           sx={{ width: "80px", height: "80px" }}
-          src="https://www.w3schools.com/howto/img_avatar.png"
+          src={auth?.user?.image}
           alt="avatar-image"
         />
         <div className="w-fit flex flex-col justify-center gap-2">
-          <h2>Shivam Sharma</h2>
-          <h4 className="font-normal">ID: 6551</h4>
+          <h2>{auth?.user?.name}</h2>
+          <h4 className="font-normal">ID: {auth?.user?.employeeId}</h4>
         </div>
       </div>
       <div className="w-[92%] h-[1px] bg-slate-400 rounded-full mx-auto mt-6"></div>
       <List component="nav" aria-labelledby="nested-list-subheader">
-        {navigationData.map((item) => (
-          <ListItemButton
-            key={item.key}
-            className="flex gap-4"
-            onClick={() => item.onClick()}
-          >
-            <ListItemIcon sx={{ minWidth: "0px" }}>{item.icon}</ListItemIcon>
-            <ListItemText primary={item.label} />
-          </ListItemButton>
-        ))}
-        {/* <ListItemButton onClick={handleClick}>
-          <ListItemText primary="Inbox" />
+        <ListItemButton onClick={handleClick} className="flex gap-4">
+          <ListItemIcon sx={{ minWidth: "0px" }}>
+            <ApprovalIcon
+              sx={{ width: "26px", height: "26px", color: "white" }}
+            />
+          </ListItemIcon>
+          <ListItemText primary="Approval Status" />
           {open ? <ExpandLess /> : <ExpandMore />}
         </ListItemButton>
         <Collapse in={open} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
             <ListItemButton sx={{ pl: 4 }}>
-              <ListItemText primary="Starred" />
+              <ListItemText primary="My Bookings" />
             </ListItemButton>
           </List>
-        </Collapse> */}
+          <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 4 }}>
+              <ListItemText primary="Approval Requests" />
+            </ListItemButton>
+          </List>
+        </Collapse>
+        {navigationData.map((item) => {
+          if (item.key === "admin" && auth?.user?.role !== "admin") {
+            return null;
+          } else {
+            return (
+              <ListItemButton
+                key={item.key}
+                className="flex gap-4"
+                onClick={() => item.onClick()}
+              >
+                <ListItemIcon sx={{ minWidth: "0px" }}>
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.label} />
+              </ListItemButton>
+            );
+          }
+        })}
       </List>
     </div>
   );
