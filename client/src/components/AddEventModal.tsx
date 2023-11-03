f01510164174dc62230bdc950623076bcaf37afb
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-03 21:55:09 +0530

    feat: add event on click

--- client/src/components/AddEventModal.tsx
+++ client/src/components/AddEventModal.tsx
@@ -23,6 +23,8 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { useAuth } from "../hooks/useAuth";
 
 import "dayjs/locale/en-gb";
+import isoToDate from "../utils/isoToDate";
+import isoToTime from "../utils/isoToTime";
 
 const AddEventModal: FC<AddEventModalProps> = ({
   isOpen,
@@ -36,7 +38,7 @@ const AddEventModal: FC<AddEventModalProps> = ({
   const [formData, setFormData] = useState<AddEventDataProps>({
     title: "",
     purpose: "",
-    date: null,
+    date: defaultDate ? dayjs(defaultDate) : null,
     start: "",
     end: "",
     slug: "",
@@ -51,8 +53,8 @@ const AddEventModal: FC<AddEventModalProps> = ({
   const slug = location.pathname.split("/")[2];
 
   const possibleTimeSlots: string[] = [];
-  const minTime = dayjs().set("hour", 8).set("minute", 0);
-  const maxTime = dayjs().set("hour", 17).set("minute", 0);
+  const minTime = dayjs().set("hour", 0).set("minute", 0);
+  const maxTime = dayjs().set("hour", 24).set("minute", 0);
   let currentTime = minTime;
 
   while (currentTime.isBefore(maxTime)) {
@@ -71,12 +73,11 @@ const AddEventModal: FC<AddEventModalProps> = ({
 
     for (const event of bookingsData) {
       if (
-        dayjs(event.date).format("YYYY-MM-DD") ===
-          dayjs(formData.date).format("YYYY-MM-DD") &&
-        ((dayjs(event.start).format("hh:mm A")! <= selectedStartTime &&
-          dayjs(event.end).format("hh:mm A") >= selectedStartTime) ||
-          (dayjs(event.start).format("hh:mm A") <= selectedEndTime &&
-            dayjs(event.end).format("hh:mm A") >= selectedEndTime))
+        isoToDate(event.date!) === isoToDate(formData.date) &&
+        ((isoToTime(event.start!) <= selectedStartTime &&
+          isoToTime(event.end!) >= selectedStartTime) ||
+          (isoToTime(event.start!) <= selectedEndTime &&
+            isoToTime(event.end!) >= selectedEndTime))
       ) {
         return true;
       }
@@ -153,166 +154,172 @@ const AddEventModal: FC<AddEventModalProps> = ({
   useEffect(() => {
     if (formData.start) {
       updateAvailableEndTimes();
+      setValidationError("");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [formData.start]);
 
+  useEffect(() => {
+    if (validationError) {
+      updateAvailableEndTimes();
+      setFormData({ ...formData, start: "", end: "" });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [validationError]);
+
   return (
-    <>
-      <Modal
-        open={isOpen}
-        onClose={() => {
-          setDefaultDate(null);
-          setIsOpen(false);
-        }}
-      >
-        <div className="bg-bgPrimary w-full max-w-[500px] px-10 py-14 absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] rounded-md flex flex-col gap-4 shadow-cardHover">
-          <Typography id="modal-modal-title" variant="h5" component="h2">
-            New event
-          </Typography>
-          <form
-            autoComplete="off"
-            className="flex flex-col gap-4"
-            onSubmit={handleSubmit}
-          >
-            <FormControl className="flex gap-4" size="small">
-              <TextField
-                id="title"
-                label="Title"
-                variant="outlined"
-                className="w-full"
-                value={formData.title}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setFormData({ ...formData, title: e.target.value })
-                }
-                required
-                size="small"
-              />
-              <TextField
-                id="purpose"
-                label="Purpose"
-                variant="outlined"
-                className="w-full"
-                value={formData.purpose}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setFormData({ ...formData, purpose: e.target.value })
-                }
-                required
-                size="small"
-                multiline
-              />
-              <LocalizationProvider
-                dateAdapter={AdapterDayjs}
-                adapterLocale="en-gb"
-              >
-                <DemoContainer
-                  components={["DatePicker"]}
-                  sx={{ padding: "0" }}
+    <Modal
+      open={isOpen}
+      onClose={() => {
+        setDefaultDate(null);
+        setIsOpen(false);
+      }}
+    >
+      <div className="bg-bgPrimary w-full max-w-[500px] px-10 py-14 absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] rounded-md flex flex-col gap-6 shadow-cardHover">
+        <Typography id="modal-modal-title" variant="h4" component="h2">
+          Create new booking
+        </Typography>
+        <form
+          autoComplete="off"
+          className="flex flex-col gap-4"
+          onSubmit={handleSubmit}
+        >
+          <FormControl className="flex gap-4" size="small">
+            <TextField
+              id="title"
+              label="Title"
+              variant="outlined"
+              className="w-full"
+              value={formData.title}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setFormData({ ...formData, title: e.target.value })
+              }
+              required
+              size="small"
+            />
+            <TextField
+              id="purpose"
+              label="Purpose"
+              variant="outlined"
+              className="w-full transition-all duration-200 ease-in"
+              value={formData.purpose}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setFormData({ ...formData, purpose: e.target.value })
+              }
+              required
+              size="small"
+              multiline
+            />
+            <LocalizationProvider
+              dateAdapter={AdapterDayjs}
+              adapterLocale="en-gb"
+            >
+              <DemoContainer components={["DatePicker"]}>
+                <DatePicker
+                  label="Enter the date"
+                  value={selectedDate && dayjs(selectedDate)}
+                  onChange={(newValue) => {
+                    setSelectedDate(newValue);
+                    setAvailableEndTimes([]);
+                    setFormData({
+                      ...formData,
+                      date: newValue ? dayjs(newValue).toISOString() : null,
+                      start: "",
+                      end: "",
+                    });
+                  }}
+                  disablePast={true}
+                  className="w-full"
+                  slotProps={{
+                    textField: {
+                      required: true,
+                      size: "small",
+                    },
+                  }}
+                />
+              </DemoContainer>
+            </LocalizationProvider>
+            <div className="flex w-full gap-2">
+              <FormControl size="small" sx={{ flex: 1 }}>
+                <InputLabel id="start-time">Start time</InputLabel>
+                <Select
+                  value={formData.start}
+                  onChange={(e: SelectChangeEvent<string | null>) => {
+                    setFormData({ ...formData, start: e.target.value });
+                  }}
+                  label="Select a start time"
+                  size="small"
+                  required
                 >
-                  <DatePicker
-                    label="Enter the date"
-                    value={selectedDate && dayjs(selectedDate)}
-                    onChange={(newValue) => {
-                      setSelectedDate(newValue);
-                      setFormData({
-                        ...formData,
-                        date: newValue ? dayjs(newValue).toISOString() : null,
-                      });
-                      console.log(formData, newValue, defaultDate);
-                    }}
-                    disablePast={true}
-                    className="w-full"
-                    slotProps={{
-                      textField: {
-                        required: true,
-                        size: "small",
-                      },
-                    }}
-                  />
-                </DemoContainer>
-              </LocalizationProvider>
-              <div className="flex w-full gap-2">
-                <FormControl size="small" sx={{ flex: 1 }}>
-                  <InputLabel id="start-time">Start time</InputLabel>
-                  <Select
-                    value={formData.start}
-                    onChange={(e: SelectChangeEvent<string | null>) => {
-                      setFormData({ ...formData, start: e.target.value });
-                    }}
-                    label="Select a start time"
-                    size="small"
-                    required
-                  >
-                    {formData.date && formData.date !== "" ? (
-                      possibleTimeSlots.map((time) => (
+                  {formData.date && formData.date !== "" ? (
+                    possibleTimeSlots.map((time) => (
+                      <MenuItem key={time} value={time}>
+                        {time}
+                      </MenuItem>
+                    ))
+                  ) : (
+                    <MenuItem value="">Please select a date</MenuItem>
+                  )}
+                </Select>
+              </FormControl>
+              <FormControl size="small" sx={{ flex: 1 }}>
+                <InputLabel id="start-time">End time</InputLabel>
+                <Select
+                  value={formData.end}
+                  onChange={(e: SelectChangeEvent<string | null>) =>
+                    setFormData({ ...formData, end: e.target.value })
+                  }
+                  label="Select an end time"
+                  size="small"
+                  required
+                >
+                  {formData.start && formData.start !== "" ? (
+                    availableEndTimes.length > 0 ? (
+                      availableEndTimes.map((time) => (
                         <MenuItem key={time} value={time}>
                           {time}
                         </MenuItem>
                       ))
                     ) : (
-                      <MenuItem value="">Please select a date</MenuItem>
-                    )}
-                  </Select>
-                </FormControl>
-                <FormControl size="small" sx={{ flex: 1 }}>
-                  <InputLabel id="start-time">End time</InputLabel>
-                  <Select
-                    value={formData.end}
-                    onChange={(e: SelectChangeEvent<string | null>) =>
-                      setFormData({ ...formData, end: e.target.value })
-                    }
-                    label="Select an end time"
-                    size="small"
-                    required
-                  >
-                    {formData.start && formData.start !== "" ? (
-                      availableEndTimes.length > 0 ? (
-                        availableEndTimes.map((time) => (
-                          <MenuItem key={time} value={time}>
-                            {time}
-                          </MenuItem>
-                        ))
-                      ) : (
-                        <MenuItem value="">No available time slots</MenuItem>
-                      )
-                    ) : (
-                      <MenuItem value="">Please select a start time</MenuItem>
-                    )}
-                  </Select>
-                </FormControl>
-              </div>
-            </FormControl>
-            <div className="w-full flex items-center justify-between mt-2">
-              <Button
-                type="submit"
-                variant="contained"
-                color="success"
-                sx={{ minWidth: "47%" }}
-                size="large"
-              >
-                Add
-              </Button>
-              <Button
-                variant="contained"
-                color="error"
-                sx={{ minWidth: "47%" }}
-                size="large"
-                onClick={() => setIsOpen(false)}
-              >
-                Cancel
-              </Button>
+                      <MenuItem value="">No available time slots</MenuItem>
+                    )
+                  ) : (
+                    <MenuItem value="">Please select a start time</MenuItem>
+                  )}
+                </Select>
+              </FormControl>
             </div>
-          </form>
-          {validationError && (
-            <Grid item xs={12}>
-              <Typography variant="body1" color="error">
-                {validationError}
-              </Typography>
-            </Grid>
-          )}
-        </div>
-      </Modal>
-    </>
+          </FormControl>
+          <div className="w-full flex items-center justify-between mt-2">
+            <Button
+              type="submit"
+              variant="contained"
+              color="success"
+              sx={{ minWidth: "47%" }}
+              size="large"
+            >
+              Add
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              sx={{ minWidth: "47%" }}
+              size="large"
+              onClick={() => setIsOpen(false)}
+            >
+              Cancel
+            </Button>
+          </div>
+        </form>
+        {validationError && (
+          <Grid item xs={12}>
+            <Typography variant="body1" color="error">
+              {validationError}
+            </Typography>
+          </Grid>
+        )}
+      </div>
+    </Modal>
   );
 };
 
