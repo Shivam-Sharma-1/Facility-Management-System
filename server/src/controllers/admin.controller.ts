ee4038471b3d80935843070b2914ce3a8236031b
Vector-ops <sumith2827@gmail.com>
2023-11-05 15:01:13 +0530

    build: add admin controller

--- server/src/controllers/admin.controller.ts
+++ server/src/controllers/admin.controller.ts
@@ -16,6 +16,15 @@ export const addFacility: RequestHandler = async (
 	try {
 		const { name, description, icon, slug, facilityManagerId } = req.body;
 
+		const facilityManager = await prisma.user.findUnique({
+			where: {
+				employeeId: facilityManagerId,
+			},
+			select: {
+				id: true,
+			},
+		});
+
 		const facility = await prisma.$transaction([
 			prisma.facility.create({
 				data: {
@@ -24,15 +33,15 @@ export const addFacility: RequestHandler = async (
 					icon,
 					slug,
 					facilityManager: {
-						connect: {
-							userId: facilityManagerId,
+						create: {
+							userId: facilityManager?.id!,
 						},
 					},
 				},
 			}),
 			prisma.user.update({
 				where: {
-					id: facilityManagerId,
+					id: facilityManager?.id!,
 				},
 				data: {
 					role: "FACILITY_MANAGER",
@@ -60,11 +69,62 @@ export const addFacility: RequestHandler = async (
 		prisma.$disconnect();
 	}
 };
+
+/**
+ * @description Delete facilities
+ * @method POST
+ * @access private
+ * @returns {Facility}
+ */
 export const deleteFacility: RequestHandler = async (
 	req: Request,
 	res: Response,
 	next: NextFunction
-) => {};
+) => {
+	try {
+		// dont delete facility manager
+		const { slug } = req.body;
+		const deletedFacility = await prisma.$transaction([
+			prisma.facility.delete({
+				where: {
+					slug,
+				},
+			}),
+			prisma.facilityManager.deleteMany({
+				where: {
+					facility: {
+						slug,
+					},
+				},
+			}),
+		]);
+		// const updateUser = await prisma.user.updateMany({
+		// 	where: {
+		// 		id: FM.map((fm) => fm.userId),
+		// 	},
+		// 	data: {
+		// 		role: "USER",
+		// 	},
+		// })
+		if (!deletedFacility) {
+			return next(
+				createHttpError.InternalServerError(
+					"Unable to delete facility. Please try again."
+				)
+			);
+		}
+		res.status(200).json(deletedFacility);
+	} catch (error) {
+		console.error(error);
+		return next(
+			createHttpError.InternalServerError(
+				"Something went wrong. Please try again."
+			)
+		);
+	} finally {
+		prisma.$disconnect();
+	}
+};
 export const updateFacility: RequestHandler = async (
 	req: Request,
 	res: Response,
