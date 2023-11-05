3be7c63f5889a3ce23f1a3b1485e2edf42872499
Vector-ops <sumith2827@gmail.com>
2023-11-05 21:24:57 +0530

    Add route for getting bookings for a facility

--- server/src/routes/facility.routes.ts
+++ server/src/routes/facility.routes.ts
@@ -8,11 +8,10 @@ import validateSession from "../middleware/validateSession";
 
 const facilityRouter = Router();
 
+facilityRouter.route("/bookings").get(validateSession, getBookingsForFacility);
 facilityRouter
 	.route("/:slug")
 	.get(validateSession, getBookings)
 	.post(validateSession, addBookings);
-
-facilityRouter.route("/bookings").get(validateSession, getBookingsForFacility);
 
 export default facilityRouter;
