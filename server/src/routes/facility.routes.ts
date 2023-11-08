ab08f3b7ec24cb888dad41691db93f9e103f8283
Vector-ops <sumith2827@gmail.com>
2023-11-08 20:09:45 +0530

    Add endpoint to get bookings for group director

--- server/src/routes/facility.routes.ts
+++ server/src/routes/facility.routes.ts
@@ -3,12 +3,14 @@ import {
 	addBookings,
 	getBookings,
 	getBookingsForFacility,
+	getBookingsForGroup,
 } from "../controllers/facility.controller";
 import validateSession from "../middleware/validateSession";
 
 const facilityRouter = Router();
 
 facilityRouter.route("/bookings").get(validateSession, getBookingsForFacility);
+facilityRouter.route("/bookings/gd").get(validateSession, getBookingsForGroup);
 facilityRouter
 	.route("/:slug")
 	.get(validateSession, getBookings)
