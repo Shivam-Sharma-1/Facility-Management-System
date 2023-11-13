8d1bf8af8c89e6517692755bb453a8e34b837fc4
Vector-ops <sumith2827@gmail.com>
2023-11-13 18:40:21 +0530

    Add role validation to validateAdmin middleware

--- server/src/middleware/validateAdmin.ts
+++ server/src/middleware/validateAdmin.ts
@@ -1,10 +1,25 @@
+import { Role } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import prisma from "../db/prisma";
 
-const validateAdmin = (req: Request, res: Response, next: NextFunction) => {
+const validateAdmin = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
 	const adminId = req.session.userId;
 
-	if (adminId !== 9999) {
+	const adminRole = await prisma.user.findFirst({
+		where: {
+			employeeId: adminId,
+		},
+		select: {
+			role: true,
+		},
+	});
+
+	if (adminRole?.role !== Role.ADMIN && adminId) {
 		return next(
 			createHttpError.Unauthorized(
 				"You are not authorized to access this resource."
