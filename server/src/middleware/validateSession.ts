485f154b13e3986f9a1bbb2eb0b4fe54d1778ae1
Vector-ops <sumith2827@gmail.com>
2023-11-13 17:20:21 +0530

    Add unauthorized error when user is not logged in

--- server/src/middleware/validateSession.ts
+++ server/src/middleware/validateSession.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const validateSession = (req: Request, res: Response, next: NextFunction) => {
 	if (!req.session.userId) {
-		return res.status(401).json({ authenticated: false });
+		return next(createHttpError.Unauthorized("You must be logged in."));
 	} else {
 		return next();
 	}
