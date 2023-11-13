f63846edcd60281494cdbaa39ee6a9476a0e5eb5
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-13 13:25:25 +0530

    style: remove loader

--- client/src/components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-const Loader = (): JSX.Element => {
-  return (
-    <div className="w-full h-full min-h-[100dvh] flex justify-center items-center">
-      <span className="loader" />
-    </div>
-  );
-};
-
-export default Loader;
