a5a5be6abbabd453822504bc1f2a56d8978e1434
Vector-ops <sumith2827@gmail.com>
2023-11-14 21:07:46 +0530

    Add additional origins to corsOptions and remove validateSession middleware

--- server/src/routes/dashboard.routes.ts
+++ server/src/routes/dashboard.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { getCount, getFacilities } from "../controllers/dashboard.controller";
-import validateSession from "../middleware/validateSession";
 
 const dashboardRouter = Router();
 
-dashboardRouter.route("/count/:employeeId").get(validateSession, getCount);
-dashboardRouter.route("/:employeeId").get(validateSession, getFacilities);
+dashboardRouter.route("/count/:employeeId").get(getCount);
+dashboardRouter.route("/:employeeId").get(getFacilities);
 
 export default dashboardRouter;
