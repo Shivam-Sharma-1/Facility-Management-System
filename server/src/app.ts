b2a0b185c182351ef8dc57399838e01a6a274c59
Vector-ops <sumith2827@gmail.com>
2023-11-14 21:07:46 +0530

    Add additional origins to corsOptions and remove validateSession middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,7 +18,12 @@ import facilityRouter from "./routes/facility.routes";
 
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-	origin: ["http://localhost:5173", `${process.env.CLIENT_URL}`],
+	origin: [
+		"http://localhost:5173",
+		`${process.env.CLIENT_URL}`,
+		"http://localhost:5000",
+		"http://localhost:8000",
+	],
 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 	credentials: true,
 };
