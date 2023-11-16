739d641e64e50a151821964e653d276a3de740f2
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-16 20:43:43 +0530

    refactor: Update user authentication process

--- server/src/controllers/cancellation.controller.ts
+++ server/src/controllers/cancellation.controller.ts
@@ -11,166 +11,162 @@ import prisma from "../db/prisma";
  */
 
 export const requestCancellation: RequestHandler = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-	try {
-		const { slug, remark, employeeId } = req.body;
+  try {
+    const { slug, remark, employeeId } = req.body;
 
-		const user = await prisma.user.findUnique({
-			where: {
-				employeeId,
-			},
-			select: {
-				role: true,
-				facilityManager: {
-					select: {
-						facility: {
-							select: {
-								slug: true,
-							},
-						},
-					},
-				},
-			},
-		});
+    const user = await prisma.user.findUnique({
+      where: {
+        employeeId,
+      },
+      select: {
+        role: true,
+        facilityManager: {
+          select: {
+            facility: {
+              select: {
+                slug: true,
+              },
+            },
+          },
+        },
+      },
+    });
 
-		const booking = await prisma.booking.findFirst({
-			where: {
-				slug,
-			},
-			include: {
-				facility: {
-					select: {
-						slug: true,
-					},
-				},
-			},
-		});
+    const booking = await prisma.booking.findFirst({
+      where: {
+        slug,
+      },
+      include: {
+        facility: {
+          select: {
+            slug: true,
+          },
+        },
+      },
+    });
 
-		if (!user || !booking) {
-			return next(
-				createHttpError.InternalServerError(
-					"Unable to request cancellation"
-				)
-			);
-		}
+    if (!user || !booking) {
+      return next(
+        createHttpError.InternalServerError("Unable to request cancellation")
+      );
+    }
 
-		let candelData = {};
+    let candelData = {};
 
-		if (
-			user?.role === Role.GROUP_DIRECTOR &&
-			booking?.status !== ApprovalStatus.APPROVED_BY_FM &&
-			booking?.status !== ApprovalStatus.APPROVED_BY_ADMIN
-		) {
-			candelData = {
-				status: ApprovalStatus.CANCELLED,
-				cancelledAt: new Date().toISOString(),
-				cancellationStatus: CancellationStatus.APPROVED_BY_GD,
-				cancellationUpdateAtGD: new Date().toISOString(),
-			};
-		} else if (user?.role === Role.GROUP_DIRECTOR) {
-			candelData = {
-				cancellationStatus: CancellationStatus.APPROVED_BY_GD,
-				cancellationUpdateAtGD: new Date().toISOString(),
-			};
-		} else if (
-			user?.role === Role.FACILITY_MANAGER &&
-			user.facilityManager?.facility.slug === booking?.facility.slug
-		) {
-			candelData = {
-				status: ApprovalStatus.CANCELLED,
-				cancelledAt: new Date().toISOString(),
-				cancellationStatus: CancellationStatus.APPROVED_BY_FM,
-				cancellationUpdateAtFM: new Date().toISOString(),
-			};
-		} else if (booking?.status === ApprovalStatus.PENDING) {
-			candelData = {
-				status: ApprovalStatus.CANCELLED,
-				cancellationStatus: CancellationStatus.CANCELLED_BY_USER,
-			};
-		} else {
-			candelData = {
-				cancellationStatus: CancellationStatus.PENDING,
-			};
-		}
+    if (
+      user?.role === Role.GROUP_DIRECTOR &&
+      booking?.status !== ApprovalStatus.APPROVED_BY_FM &&
+      booking?.status !== ApprovalStatus.APPROVED_BY_ADMIN
+    ) {
+      candelData = {
+        status: ApprovalStatus.CANCELLED,
+        cancelledAt: new Date().toISOString(),
+        cancellationStatus: CancellationStatus.APPROVED_BY_GD,
+        cancellationUpdateAtGD: new Date().toISOString(),
+      };
+    } else if (user?.role === Role.GROUP_DIRECTOR) {
+      candelData = {
+        cancellationStatus: CancellationStatus.APPROVED_BY_GD,
+        cancellationUpdateAtGD: new Date().toISOString(),
+      };
+    } else if (
+      user?.role === Role.FACILITY_MANAGER &&
+      user.facilityManager?.facility.slug === booking?.facility.slug
+    ) {
+      candelData = {
+        status: ApprovalStatus.CANCELLED,
+        cancelledAt: new Date().toISOString(),
+        cancellationStatus: CancellationStatus.APPROVED_BY_FM,
+        cancellationUpdateAtFM: new Date().toISOString(),
+      };
+    } else if (booking?.status === ApprovalStatus.PENDING) {
+      candelData = {
+        status: ApprovalStatus.CANCELLED,
+        cancellationStatus: CancellationStatus.CANCELLED_BY_USER,
+      };
+    } else {
+      candelData = {
+        cancellationStatus: CancellationStatus.PENDING,
+      };
+    }
 
-		const cancellationBooking = await prisma.booking.update({
-			where: {
-				slug,
-			},
-			data: {
-				...candelData,
-				cancellationRequestedAt: new Date().toISOString(),
-				cancellationRemark: remark,
-			},
-			select: {
-				title: true,
-				purpose: true,
-				status: true,
-				slug: true,
-				createdAt: true,
-				remark: true,
-				cancellationStatus: true,
-				cancellationRequestedAt: true,
-				cancellationRemark: true,
-				cancelledAt: true,
-				time: {
-					select: {
-						start: true,
-						end: true,
-						date: true,
-					},
-				},
-				statusUpdateAtGD: true,
-				statusUpdateAtFM: true,
-				statusUpdateAtAdmin: true,
-				requestedBy: {
-					select: {
-						name: true,
-						employeeId: true,
-					},
-				},
-				facility: {
-					select: {
-						name: true,
-					},
-				},
-				statusUpdateByFM: {
-					select: {
-						user: {
-							select: {
-								name: true,
-								employeeId: true,
-							},
-						},
-					},
-				},
-				statusUpdateByGD: {
-					select: {
-						user: {
-							select: {
-								name: true,
-								employeeId: true,
-							},
-						},
-					},
-				},
-			},
-		});
-		if (!cancellationBooking) {
-			return next(createHttpError.NotFound("Booking not found"));
-		}
-		res.status(200).json(cancellationBooking);
-	} catch (error) {
-		console.error(error);
-		return next(
-			createHttpError.InternalServerError(
-				"Unable to request cancellation"
-			)
-		);
-	}
+    const cancellationBooking = await prisma.booking.update({
+      where: {
+        slug,
+      },
+      data: {
+        ...candelData,
+        cancellationRequestedAt: new Date().toISOString(),
+        cancellationRemark: remark,
+      },
+      select: {
+        title: true,
+        purpose: true,
+        status: true,
+        slug: true,
+        createdAt: true,
+        remark: true,
+        cancellationStatus: true,
+        cancellationRequestedAt: true,
+        cancellationRemark: true,
+        cancelledAt: true,
+        time: {
+          select: {
+            start: true,
+            end: true,
+            date: true,
+          },
+        },
+        statusUpdateAtGD: true,
+        statusUpdateAtFM: true,
+        statusUpdateAtAdmin: true,
+        requestedBy: {
+          select: {
+            name: true,
+            employeeId: true,
+          },
+        },
+        facility: {
+          select: {
+            name: true,
+          },
+        },
+        statusUpdateByFM: {
+          select: {
+            user: {
+              select: {
+                name: true,
+                employeeId: true,
+              },
+            },
+          },
+        },
+        statusUpdateByGD: {
+          select: {
+            user: {
+              select: {
+                name: true,
+                employeeId: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    if (!cancellationBooking) {
+      return next(createHttpError.NotFound("Booking not found"));
+    }
+    res.status(200).json(cancellationBooking);
+  } catch (error) {
+    console.error(error);
+    return next(
+      createHttpError.InternalServerError("Unable to request cancellation")
+    );
+  }
 };
 
 /**
@@ -181,78 +177,76 @@ export const requestCancellation: RequestHandler = async (
  */
 
 export const getAllCancellationRequestsGD: RequestHandler = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-	try {
-		const employeeId = req.session.userId;
+  try {
+    const employeeId = req.session.userId;
 
-		const cancellationRequests = await prisma.booking.findMany({
-			where: {
-				AND: [
-					{
-						group: {
-							groupDirector: {
-								user: {
-									employeeId,
-								},
-							},
-						},
-					},
-					{
-						cancellationStatus: "PENDING",
-					},
-				],
-			},
-			select: {
-				id: true,
-				title: true,
-				slug: true,
-				purpose: true,
-				status: true,
-				createdAt: true,
-				cancellationRequestedAt: true,
-				cancellationStatus: true,
-				cancellationRemark: true,
-				cancellationUpdateAtGD: true,
-				time: {
-					select: {
-						start: true,
-						end: true,
-						date: true,
-					},
-				},
-				requestedBy: {
-					select: {
-						name: true,
-						employeeId: true,
-					},
-				},
-				facility: {
-					select: {
-						name: true,
-						slug: true,
-					},
-				},
-			},
-		});
-		if (!cancellationRequests) {
-			return next(
-				createHttpError.Forbidden(
-					"You do not have access to this resource"
-				)
-			);
-		}
-		res.status(200).json(cancellationRequests);
-	} catch (error) {
-		console.error(error);
-		return next(
-			createHttpError.InternalServerError(
-				"Unable to fetch cancellation requests"
-			)
-		);
-	}
+    const cancellationRequests = await prisma.booking.findMany({
+      where: {
+        AND: [
+          {
+            group: {
+              groupDirector: {
+                user: {
+                  employeeId,
+                },
+              },
+            },
+          },
+          {
+            cancellationStatus: "PENDING",
+          },
+        ],
+      },
+      select: {
+        id: true,
+        title: true,
+        slug: true,
+        purpose: true,
+        status: true,
+        createdAt: true,
+        cancellationRequestedAt: true,
+        cancellationStatus: true,
+        cancellationRemark: true,
+        cancellationUpdateAtGD: true,
+        time: {
+          select: {
+            start: true,
+            end: true,
+            date: true,
+          },
+        },
+        requestedBy: {
+          select: {
+            name: true,
+            employeeId: true,
+          },
+        },
+        facility: {
+          select: {
+            name: true,
+            slug: true,
+          },
+        },
+      },
+    });
+    if (!cancellationRequests) {
+      return next(
+        createHttpError.Forbidden("You do not have access to this resource")
+      );
+    }
+    res.status(200).json(cancellationRequests);
+  } catch (error) {
+    console.error(error);
+    return next(
+      createHttpError.InternalServerError(
+        "Unable to fetch cancellation requests"
+      )
+    );
+  }
 };
 
 /**
@@ -263,98 +257,95 @@ export const getAllCancellationRequestsGD: RequestHandler = async (
  */
 
 export const getAllCancellationRequestsFM: RequestHandler = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-	try {
-		const employeeId = req.session.userId;
-		const cancellationRequests = await prisma.facilityManager.findFirst({
-			where: {
-				user: {
-					employeeId,
-				},
-			},
-			select: {
-				facility: {
-					select: {
-						bookings: {
-							where: {
-								AND: [
-									{
-										cancellationStatus:
-											CancellationStatus.APPROVED_BY_GD,
-									},
-									{
-										status:
-											ApprovalStatus.APPROVED_BY_FM ||
-											ApprovalStatus.APPROVED_BY_ADMIN,
-									},
-								],
-							},
-							select: {
-								id: true,
-								slug: true,
-								title: true,
-								purpose: true,
-								status: true,
-								createdAt: true,
-								cancellationRemark: true,
-								cancellationRequestedAt: true,
-								cancellationStatus: true,
-								cancellationUpdateAtGD: true,
-								cancellationUpdateAtFM: true,
-								time: {
-									select: {
-										start: true,
-										end: true,
-										date: true,
-									},
-								},
-								requestedBy: {
-									select: {
-										name: true,
-										employeeId: true,
-									},
-								},
-								facility: {
-									select: {
-										name: true,
-										slug: true,
-									},
-								},
-								statusUpdateByGD: {
-									select: {
-										user: {
-											select: {
-												name: true,
-												employeeId: true,
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		});
-		if (!cancellationRequests) {
-			return next(
-				createHttpError.Forbidden(
-					"You do not have access to this resource"
-				)
-			);
-		}
-		res.status(200).json(cancellationRequests.facility.bookings);
-	} catch (error) {
-		console.error(error);
-		return next(
-			createHttpError.InternalServerError(
-				"Unable to fetch cancellation requests"
-			)
-		);
-	}
+  try {
+    const employeeId = req.session.userId;
+    const cancellationRequests = await prisma.facilityManager.findFirst({
+      where: {
+        user: {
+          employeeId,
+        },
+      },
+      select: {
+        facility: {
+          select: {
+            bookings: {
+              where: {
+                AND: [
+                  {
+                    cancellationStatus: CancellationStatus.APPROVED_BY_GD,
+                  },
+                  {
+                    status:
+                      ApprovalStatus.APPROVED_BY_FM ||
+                      ApprovalStatus.APPROVED_BY_ADMIN,
+                  },
+                ],
+              },
+              select: {
+                id: true,
+                slug: true,
+                title: true,
+                purpose: true,
+                status: true,
+                createdAt: true,
+                cancellationRemark: true,
+                cancellationRequestedAt: true,
+                cancellationStatus: true,
+                cancellationUpdateAtGD: true,
+                cancellationUpdateAtFM: true,
+                time: {
+                  select: {
+                    start: true,
+                    end: true,
+                    date: true,
+                  },
+                },
+                requestedBy: {
+                  select: {
+                    name: true,
+                    employeeId: true,
+                  },
+                },
+                facility: {
+                  select: {
+                    name: true,
+                    slug: true,
+                  },
+                },
+                statusUpdateByGD: {
+                  select: {
+                    user: {
+                      select: {
+                        name: true,
+                        employeeId: true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+    if (!cancellationRequests) {
+      return next(
+        createHttpError.Forbidden("You do not have access to this resource")
+      );
+    }
+    res.status(200).json(cancellationRequests.facility.bookings);
+  } catch (error) {
+    console.error(error);
+    return next(
+      createHttpError.InternalServerError(
+        "Unable to fetch cancellation requests"
+      )
+    );
+  }
 };
 
 /**
@@ -365,41 +356,52 @@ export const getAllCancellationRequestsFM: RequestHandler = async (
  */
 
 export const approveCancellationRequestGD: RequestHandler = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-	try {
-		const { slug, approved } = req.body;
-		const status = approved
-			? CancellationStatus.APPROVED_BY_GD
-			: CancellationStatus.REJECTED_BY_GD;
+  try {
+    const { slug, approved } = req.body;
+    const status = approved
+      ? CancellationStatus.APPROVED_BY_GD
+      : CancellationStatus.REJECTED_BY_GD;
 
-		const cancellationRequest = await prisma.booking.update({
-			where: {
-				slug,
-			},
-			data: {
-				cancellationStatus: status,
-				cancellationUpdateAtGD: new Date().toISOString(),
-			},
-		});
+    const bookingStatus = await prisma.booking.findUnique({
+      where: {
+        slug,
+      },
+      select: {
+        status: true,
+      },
+    });
 
-		if (!cancellationRequest) {
-			return next(
-				createHttpError.NotFound("Cancellation request not found")
-			);
-		}
+    const cancellationRequest = await prisma.booking.update({
+      where: {
+        slug,
+      },
+      data: {
+        status:
+          bookingStatus?.status === ApprovalStatus.APPROVED_BY_GD
+            ? ApprovalStatus.CANCELLED
+            : bookingStatus?.status,
+        cancellationStatus: status,
+        cancellationUpdateAtGD: new Date().toISOString(),
+      },
+    });
 
-		res.status(200).json(cancellationRequest);
-	} catch (error) {
-		console.error(error);
-		return next(
-			createHttpError.InternalServerError(
-				"Unable to approve cancellation request"
-			)
-		);
-	}
+    if (!cancellationRequest) {
+      return next(createHttpError.NotFound("Cancellation request not found"));
+    }
+
+    res.status(200).json(cancellationRequest);
+  } catch (error) {
+    console.error(error);
+    return next(
+      createHttpError.InternalServerError(
+        "Unable to approve cancellation request"
+      )
+    );
+  }
 };
 
 /**
@@ -410,49 +412,47 @@ export const approveCancellationRequestGD: RequestHandler = async (
  */
 
 export const approveCancellationRequestFM: RequestHandler = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-	try {
-		const { slug, approved } = req.body;
-		let status = {};
+  try {
+    const { slug, approved } = req.body;
+    let status = {};
 
-		if (approved === true) {
-			status = {
-				cancellationStatus: CancellationStatus.APPROVED_BY_FM,
-				status: ApprovalStatus.CANCELLED,
-			};
-		} else {
-			status = {
-				cancellationStatus: CancellationStatus.REJECTED_BY_FM,
-			};
-		}
+    if (approved === true) {
+      status = {
+        cancellationStatus: CancellationStatus.APPROVED_BY_FM,
+        status: ApprovalStatus.CANCELLED,
+      };
+    } else {
+      status = {
+        cancellationStatus: CancellationStatus.REJECTED_BY_FM,
+      };
+    }
 
-		const cancellationRequest = await prisma.booking.update({
-			where: {
-				slug,
-			},
-			data: {
-				...status,
-				cancellationUpdateAtFM: new Date().toISOString(),
-				cancelledAt: new Date().toISOString(),
-			},
-		});
+    const cancellationRequest = await prisma.booking.update({
+      where: {
+        slug,
+      },
+      data: {
+        ...status,
+        cancellationUpdateAtFM: new Date().toISOString(),
+        cancelledAt: new Date().toISOString(),
+      },
+    });
 
-		if (!cancellationRequest) {
-			return next(
-				createHttpError.NotFound("Cancellation request not found")
-			);
-		}
+    if (!cancellationRequest) {
+      return next(createHttpError.NotFound("Cancellation request not found"));
+    }
 
-		res.status(200).json(cancellationRequest);
-	} catch (error) {
-		console.error(error);
-		return next(
-			createHttpError.InternalServerError(
-				"Unable to approve cancellation request"
-			)
-		);
-	}
+    res.status(200).json(cancellationRequest);
+  } catch (error) {
+    console.error(error);
+    return next(
+      createHttpError.InternalServerError(
+        "Unable to approve cancellation request"
+      )
+    );
+  }
 };
