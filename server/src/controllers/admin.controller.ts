cd25df496186d5cbc57bb684f0adabc4f0a61440
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-16 20:43:22 +0530

    refactor: code to improve performance and readability

--- server/src/controllers/admin.controller.ts
+++ server/src/controllers/admin.controller.ts
@@ -3,40 +3,50 @@ import createHttpError from "http-errors";
 import prisma from "../db/prisma";
 
 /**
- * @description Get facilities for filtering
+ * @description Get facilities for filtering in bookings
  * @method GET
  * @access private
  * @returns {Facility}
  */
 export const getFacilities: RequestHandler = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-	try {
-		const facilities = await prisma.facility.findMany({
-			include: {
-				facilityManager: {
-					select: {
-						user: {
-							select: {
-								employeeId: true,
-								name: true,
-							},
-						},
-					},
-				},
-			},
-		});
-		res.status(200).json(facilities);
-	} catch (error) {
-		console.error(error);
-		return next(
-			createHttpError.InternalServerError(
-				"Something went wrong. Please try again."
-			)
-		);
-	}
+  try {
+    const buildings = await prisma.building.findMany({
+      select: {
+        name: true,
+      },
+    });
+    const facilities = await prisma.facility.findMany({
+      include: {
+        facilityManager: {
+          select: {
+            user: {
+              select: {
+                employeeId: true,
+                name: true,
+              },
+            },
+          },
+        },
+        building: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+    res.status(200).json({ buildings, facilities });
+  } catch (error) {
+    console.error(error);
+    return next(
+      createHttpError.InternalServerError(
+        "Something went wrong. Please try again."
+      )
+    );
+  }
 };
 
 /**
@@ -46,63 +56,69 @@ export const getFacilities: RequestHandler = async (
  * @returns {Facility}
  */
 export const addFacility: RequestHandler = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-	try {
-		const { name, description, icon, slug, facilityManagerId } = req.body;
+  try {
+    const { name, description, icon, slug, facilityManagerId, building } =
+      req.body;
 
-		const facilityManager = await prisma.user.findUnique({
-			where: {
-				employeeId: facilityManagerId,
-			},
-			select: {
-				id: true,
-			},
-		});
+    const facilityManager = await prisma.user.findUnique({
+      where: {
+        employeeId: facilityManagerId,
+      },
+      select: {
+        id: true,
+      },
+    });
 
-		const facility = await prisma.$transaction([
-			prisma.facility.create({
-				data: {
-					name,
-					description,
-					icon,
-					slug,
-					facilityManager: {
-						create: {
-							userId: facilityManager?.id!,
-						},
-					},
-				},
-			}),
-			prisma.user.update({
-				where: {
-					id: facilityManager?.id!,
-				},
-				data: {
-					role: "FACILITY_MANAGER",
-				},
-			}),
-		]);
+    const facility = await prisma.$transaction([
+      prisma.facility.create({
+        data: {
+          name,
+          description,
+          icon,
+          slug,
+          facilityManager: {
+            create: {
+              userId: facilityManager?.id!,
+            },
+          },
+          building: {
+            connect: {
+              name: building,
+            },
+          },
+        },
+      }),
+      prisma.user.update({
+        where: {
+          id: facilityManager?.id!,
+        },
+        data: {
+          role: "FACILITY_MANAGER",
+        },
+      }),
+    ]);
 
-		if (!facility) {
-			return next(
-				createHttpError.InternalServerError(
-					"Something went wrong. Please try again."
-				)
-			);
-		}
+    if (!facility) {
+      return next(
+        createHttpError.InternalServerError(
+          "Something went wrong. Please try again."
+        )
+      );
+    }
 
-		res.status(200).json(facility);
-	} catch (error) {
-		console.error(error);
-		return next(
-			createHttpError.InternalServerError(
-				"Something went wrong. Please try again."
-			)
-		);
-	}
+    res.status(200).json(facility);
+  } catch (error) {
+    console.error(error);
+    return next(
+      createHttpError.InternalServerError(
+        "Something went wrong. Please try again."
+      )
+    );
+  }
 };
 
 /**
@@ -112,64 +128,64 @@ export const addFacility: RequestHandler = async (
  * @returns {Facility}
  */
 export const deleteFacility: RequestHandler = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-	try {
-		const { slug } = req.body;
-		const time = new Date().toISOString();
-		const facilityManager = await prisma.facilityManager.findFirst({
-			where: {
-				facility: {
-					slug,
-				},
-			},
-			select: {
-				userId: true,
-			},
-		});
-		const deletedFacility = await prisma.$transaction([
-			prisma.facility.update({
-				where: {
-					slug,
-				},
-				data: {
-					isActive: false,
-					deletedAt: time,
-				},
-			}),
-			prisma.facilityManager.delete({
-				where: {
-					userId: facilityManager?.userId,
-				},
-			}),
-			prisma.user.update({
-				where: {
-					id: facilityManager?.userId,
-				},
-				data: {
-					role: "USER",
-				},
-			}),
-		]);
+  try {
+    const { slug } = req.body;
+    const time = new Date().toISOString();
+    const facilityManager = await prisma.facilityManager.findFirst({
+      where: {
+        facility: {
+          slug,
+        },
+      },
+      select: {
+        userId: true,
+      },
+    });
+    const deletedFacility = await prisma.$transaction([
+      prisma.facility.update({
+        where: {
+          slug,
+        },
+        data: {
+          isActive: false,
+          deletedAt: time,
+        },
+      }),
+      prisma.facilityManager.delete({
+        where: {
+          userId: facilityManager?.userId,
+        },
+      }),
+      prisma.user.update({
+        where: {
+          id: facilityManager?.userId,
+        },
+        data: {
+          role: "USER",
+        },
+      }),
+    ]);
 
-		if (!deletedFacility) {
-			return next(
-				createHttpError.InternalServerError(
-					"Unable to delete facility. Please try again."
-				)
-			);
-		}
-		res.status(200).json(deletedFacility);
-	} catch (error) {
-		console.error(error);
-		return next(
-			createHttpError.InternalServerError(
-				"Something went wrong. Please try again."
-			)
-		);
-	}
+    if (!deletedFacility) {
+      return next(
+        createHttpError.InternalServerError(
+          "Unable to delete facility. Please try again."
+        )
+      );
+    }
+    res.status(200).json(deletedFacility);
+  } catch (error) {
+    console.error(error);
+    return next(
+      createHttpError.InternalServerError(
+        "Something went wrong. Please try again."
+      )
+    );
+  }
 };
 
 /**
@@ -180,80 +196,82 @@ export const deleteFacility: RequestHandler = async (
  */
 
 export const updateFacility: RequestHandler = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-	try {
-		const {
-			slug,
-			name,
-			description,
-			newFacilityManagerId,
-			prevFacilityManagerId,
-			icon,
-		} = req.body;
-		const prevFacilityManager = await prisma.facilityManager.findFirst({
-			where: {
-				user: {
-					employeeId: prevFacilityManagerId,
-				},
-			},
-			select: {
-				userId: true,
-			},
-		});
-		const facility = await prisma.$transaction([
-			prisma.facility.update({
-				where: {
-					slug,
-				},
-				data: {
-					name,
-					description,
-					icon,
-				},
-			}),
-			prisma.facilityManager.delete({
-				where: {
-					userId: prevFacilityManager?.userId,
-				},
-			}),
-			prisma.user.update({
-				where: {
-					employeeId: prevFacilityManagerId,
-				},
-				data: {
-					role: "USER",
-				},
-			}),
-			prisma.user.update({
-				where: {
-					employeeId: newFacilityManagerId,
-				},
-				data: {
-					role: "FACILITY_MANAGER",
-					facilityManager: {
-						create: {
-							facility: {
-								connect: {
-									slug,
-								},
-							},
-						},
-					},
-				},
-			}),
-		]);
-		res.status(201).json(facility);
-	} catch (error) {
-		console.error(error);
-		return next(
-			createHttpError.InternalServerError(
-				"Something went wrong. Please try again."
-			)
-		);
-	}
+  try {
+    const {
+      slug,
+      name,
+      description,
+      newFacilityManagerId,
+      prevFacilityManagerId,
+      icon,
+      building,
+    } = req.body;
+    const prevFacilityManager = await prisma.facilityManager.findFirst({
+      where: {
+        user: {
+          employeeId: prevFacilityManagerId,
+        },
+      },
+      select: {
+        userId: true,
+      },
+    });
+    const facility = await prisma.$transaction([
+      prisma.facility.update({
+        where: {
+          slug,
+        },
+        data: {
+          name,
+          description,
+          icon,
+          building,
+        },
+      }),
+      prisma.facilityManager.delete({
+        where: {
+          userId: prevFacilityManager?.userId,
+        },
+      }),
+      prisma.user.update({
+        where: {
+          employeeId: prevFacilityManagerId,
+        },
+        data: {
+          role: "USER",
+        },
+      }),
+      prisma.user.update({
+        where: {
+          employeeId: newFacilityManagerId,
+        },
+        data: {
+          role: "FACILITY_MANAGER",
+          facilityManager: {
+            create: {
+              facility: {
+                connect: {
+                  slug,
+                },
+              },
+            },
+          },
+        },
+      }),
+    ]);
+    res.status(201).json(facility);
+  } catch (error) {
+    console.error(error);
+    return next(
+      createHttpError.InternalServerError(
+        "Something went wrong. Please try again."
+      )
+    );
+  }
 };
 
 /**
@@ -264,120 +282,120 @@ export const updateFacility: RequestHandler = async (
  */
 
 export const getAllBookings: RequestHandler = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-	try {
-		const { month, facility } = req.query;
-		let filterConditions = {};
+  try {
+    const { month, facility } = req.query;
+    let filterConditions = {};
 
-		if (month) {
-			// Parse the month parameter as a Date object
-			const startDate = new Date(`${month}-01`);
-			const endDate = new Date(`${month}-31`);
-			startDate.setFullYear(new Date().getFullYear());
-			endDate.setFullYear(new Date().getFullYear());
+    if (month) {
+      // Parse the month parameter as a Date object
+      const startDate = new Date(`${month}-01`);
+      const endDate = new Date(`${month}-31`);
+      startDate.setFullYear(new Date().getFullYear());
+      endDate.setFullYear(new Date().getFullYear());
 
-			filterConditions = {
-				...filterConditions,
+      filterConditions = {
+        ...filterConditions,
 
-				createdAt: {
-					gte: startDate,
-					lt: endDate,
-				},
-			};
-		}
+        createdAt: {
+          gte: startDate,
+          lt: endDate,
+        },
+      };
+    }
 
-		if (facility) {
-			filterConditions = {
-				AND: [
-					{ ...filterConditions },
-					{
-						facility: {
-							slug: facility,
-						},
-					},
-				],
-			};
-		}
+    if (facility) {
+      filterConditions = {
+        AND: [
+          { ...filterConditions },
+          {
+            facility: {
+              slug: facility,
+            },
+          },
+        ],
+      };
+    }
 
-		const bookings = await prisma.booking.findMany({
-			where: filterConditions,
-			select: {
-				title: true,
-				purpose: true,
-				status: true,
-				slug: true,
-				createdAt: true,
-				remark: true,
-				cancellationStatus: true,
-				cancellationRemark: true,
-				cancelledAt: true,
-				time: {
-					select: {
-						start: true,
-						end: true,
-						date: true,
-					},
-				},
-				statusUpdateAtGD: true,
-				statusUpdateAtFM: true,
-				statusUpdateAtAdmin: true,
-				requestedBy: {
-					select: {
-						name: true,
-						employeeId: true,
-					},
-				},
-				facility: {
-					select: {
-						name: true,
-					},
-				},
-				statusUpdateByFM: {
-					select: {
-						user: {
-							select: {
-								name: true,
-								employeeId: true,
-							},
-						},
-					},
-				},
-				statusUpdateByGD: {
-					select: {
-						user: {
-							select: {
-								name: true,
-								employeeId: true,
-							},
-						},
-					},
-				},
-			},
-			orderBy: {
-				createdAt: "desc",
-			},
-		});
-		const facilities = await prisma.facility.findMany({
-			where: {
-				isActive: true,
-			},
-			select: {
-				slug: true,
-				name: true,
-			},
-		});
-		res.status(200).json({ facilities, bookings });
-	} catch (error) {
-		console.error(error.message);
-		return next(
-			createHttpError.InternalServerError(
-				"Something went wrong. Please try again."
-			)
-		);
-	}
+    const bookings = await prisma.booking.findMany({
+      where: filterConditions,
+      select: {
+        title: true,
+        purpose: true,
+        status: true,
+        slug: true,
+        createdAt: true,
+        remark: true,
+        cancellationStatus: true,
+        cancellationRemark: true,
+        cancelledAt: true,
+        time: {
+          select: {
+            start: true,
+            end: true,
+            date: true,
+          },
+        },
+        statusUpdateAtGD: true,
+        statusUpdateAtFM: true,
+        statusUpdateAtAdmin: true,
+        requestedBy: {
+          select: {
+            name: true,
+            employeeId: true,
+          },
+        },
+        facility: {
+          select: {
+            name: true,
+          },
+        },
+        statusUpdateByFM: {
+          select: {
+            user: {
+              select: {
+                name: true,
+                employeeId: true,
+              },
+            },
+          },
+        },
+        statusUpdateByGD: {
+          select: {
+            user: {
+              select: {
+                name: true,
+                employeeId: true,
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    const facilities = await prisma.facility.findMany({
+      where: {
+        isActive: true,
+      },
+      select: {
+        slug: true,
+        name: true,
+      },
+    });
+    res.status(200).json({ facilities, bookings });
+  } catch (error) {
+    console.error(error.message);
+    return next(
+      createHttpError.InternalServerError(
+        "Something went wrong. Please try again."
+      )
+    );
+  }
 };
 
 /**
@@ -388,38 +406,36 @@ export const getAllBookings: RequestHandler = async (
  */
 
 export const approveBooking: RequestHandler = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-	try {
-		const { slug, approved, remark } = req.body;
-		const time = new Date().toISOString();
+  try {
+    const { slug, approved, remark } = req.body;
+    const time = new Date().toISOString();
 
-		const booking = await prisma.booking.update({
-			where: {
-				slug,
-			},
-			data: {
-				status: approved ? "APPROVED_BY_ADMIN" : "REJECTED_BY_ADMIN",
-				remark,
-				statusUpdateAtAdmin: time,
-			},
-		});
-		if (!booking) {
-			return next(
-				createHttpError.BadRequest(
-					"Something went wrong. Please try again."
-				)
-			);
-		}
-		res.status(200).json(booking);
-	} catch (error) {
-		console.error(error);
-		return next(
-			createHttpError.InternalServerError(
-				"Something went wrong. Please try again."
-			)
-		);
-	}
+    const booking = await prisma.booking.update({
+      where: {
+        slug,
+      },
+      data: {
+        status: approved ? "APPROVED_BY_ADMIN" : "REJECTED_BY_ADMIN",
+        remark,
+        statusUpdateAtAdmin: time,
+      },
+    });
+    if (!booking) {
+      return next(
+        createHttpError.BadRequest("Something went wrong. Please try again.")
+      );
+    }
+    res.status(200).json(booking);
+  } catch (error) {
+    console.error(error);
+    return next(
+      createHttpError.InternalServerError(
+        "Something went wrong. Please try again."
+      )
+    );
+  }
 };
