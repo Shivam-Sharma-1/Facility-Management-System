da05eea33a8c3bda8dea3cf9905f6c194b4475b6
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-20 22:33:40 +0530

    refactor: routes in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,10 @@ import "@tanstack/react-query";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import {
-	Route,
-	RouterProvider,
-	createBrowserRouter,
-	createRoutesFromElements,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
 } from "react-router-dom";
 
 import Layout from "./components/Layout";
@@ -29,192 +29,173 @@ import SetUser from "./pages/SetUser";
 import { AuthProvider } from "./utils/auth";
 
 declare module "@tanstack/react-query" {
-	interface Register {
-		defaultError: AxiosError;
-	}
+  interface Register {
+    defaultError: AxiosError;
+  }
 }
 
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<>
-			<Route path="/auth">
-				<Route
-					index
-					element={<SetUser />}
-					errorElement={<RouteError />}
-				/>
-				<Route
-					path="login"
-					element={<LoginPage />}
-					errorElement={<RouteError />}
-				/>
-				<Route
-					path="reset-password"
-					element={
-						<RequireAuth GD={false} FM={false} Admin={true}>
-							<ResetPasswordPage />
-						</RequireAuth>
-					}
-					errorElement={<RouteError />}
-				/>
-			</Route>
+  createRoutesFromElements(
+    <>
+      <Route path="/auth">
+        <Route index element={<SetUser />} errorElement={<RouteError />} />
+        <Route
+          path="login"
+          element={<LoginPage />}
+          errorElement={<RouteError />}
+        />
+        <Route
+          path="reset-password"
+          element={
+            <RequireAuth GD={false} FM={false} Admin={true}>
+              <ResetPasswordPage />
+            </RequireAuth>
+          }
+          errorElement={<RouteError />}
+        />
+      </Route>
 
-			<Route
-				path="/"
-				element={
-					<RequireAuth GD={false} FM={false}>
-						<Layout />
-					</RequireAuth>
-				}
-				errorElement={<RouteError />}
-			>
-				<Route
-					index
-					element={
-						<RequireAuth GD={false} FM={false} noAdmin={true}>
-							<DashboardPage />
-						</RequireAuth>
-					}
-					errorElement={<RouteError />}
-				/>
+      <Route
+        path="/"
+        element={
+          <RequireAuth GD={false} FM={false}>
+            <Layout />
+          </RequireAuth>
+        }
+        errorElement={<RouteError />}
+      >
+        <Route
+          index
+          element={
+            <RequireAuth GD={false} FM={false} noAdmin={true}>
+              <DashboardPage />
+            </RequireAuth>
+          }
+          errorElement={<RouteError />}
+        />
 
-				<Route path="facility">
-					<Route
-						path=":id"
-						element={
-							<RequireAuth GD={false} FM={false} noAdmin={true}>
-								<FacilityPage />
-							</RequireAuth>
-						}
-					/>
-				</Route>
+        <Route path="facility">
+          <Route
+            path=":id"
+            element={
+              <RequireAuth GD={false} FM={false} noAdmin={true}>
+                <FacilityPage />
+              </RequireAuth>
+            }
+            errorElement={<RouteError />}
+          />
+        </Route>
 
-				<Route path="bookings">
-					<Route
-						path="gd"
-						element={
-							<RequireAuth GD={true} FM={false} noAdmin={true}>
-								<GDBookingsPage />
-							</RequireAuth>
-						}
-						errorElement={<RouteError />}
-					/>
-					<Route
-						path="fm"
-						element={
-							<RequireAuth GD={false} FM={true} noAdmin={true}>
-								<FMBookingsPage />
-							</RequireAuth>
-						}
-						errorElement={<RouteError />}
-					/>
-				</Route>
+        <Route path="bookings">
+          <Route
+            path="gd"
+            element={
+              <RequireAuth GD={true} FM={false} noAdmin={true}>
+                <GDBookingsPage />
+              </RequireAuth>
+            }
+            errorElement={<RouteError />}
+          />
+          <Route
+            path="fm"
+            element={
+              <RequireAuth GD={false} FM={true} noAdmin={true}>
+                <FMBookingsPage />
+              </RequireAuth>
+            }
+            errorElement={<RouteError />}
+          />
+        </Route>
 
-				<Route path="employee">
-					<Route path="approvals">
-						<Route
-							path="gd"
-							element={
-								<RequireAuth
-									GD={true}
-									FM={false}
-									noAdmin={true}
-								>
-									<GDApprovalsPage />
-								</RequireAuth>
-							}
-							errorElement={<RouteError />}
-						/>
-						<Route
-							path="fm"
-							element={
-								<RequireAuth
-									GD={false}
-									FM={true}
-									noAdmin={true}
-								>
-									<FMApprovalsPage />
-								</RequireAuth>
-							}
-							errorElement={<RouteError />}
-						/>
-					</Route>
+        <Route path="employee">
+          <Route path="approvals">
+            <Route
+              path="gd"
+              element={
+                <RequireAuth GD={true} FM={false} noAdmin={true}>
+                  <GDApprovalsPage />
+                </RequireAuth>
+              }
+              errorElement={<RouteError />}
+            />
+            <Route
+              path="fm"
+              element={
+                <RequireAuth GD={false} FM={true} noAdmin={true}>
+                  <FMApprovalsPage />
+                </RequireAuth>
+              }
+              errorElement={<RouteError />}
+            />
+          </Route>
 
-					<Route path="cancellations">
-						<Route
-							path="gd"
-							element={
-								<RequireAuth
-									GD={true}
-									FM={false}
-									noAdmin={true}
-								>
-									<GDCancellationsPage />
-								</RequireAuth>
-							}
-							errorElement={<RouteError />}
-						/>
-						<Route
-							path="fm"
-							element={
-								<RequireAuth
-									GD={false}
-									FM={true}
-									noAdmin={true}
-								>
-									<FMCancellationsPage />
-								</RequireAuth>
-							}
-							errorElement={<RouteError />}
-						/>
-					</Route>
+          <Route path="cancellations">
+            <Route
+              path="gd"
+              element={
+                <RequireAuth GD={true} FM={false} noAdmin={true}>
+                  <GDCancellationsPage />
+                </RequireAuth>
+              }
+              errorElement={<RouteError />}
+            />
+            <Route
+              path="fm"
+              element={
+                <RequireAuth GD={false} FM={true} noAdmin={true}>
+                  <FMCancellationsPage />
+                </RequireAuth>
+              }
+              errorElement={<RouteError />}
+            />
+          </Route>
 
-					<Route
-						path="mybookings"
-						element={
-							<RequireAuth GD={false} FM={false} noAdmin={true}>
-								<MyBookingsPage />
-							</RequireAuth>
-						}
-						errorElement={<RouteError />}
-					/>
-				</Route>
+          <Route
+            path="mybookings"
+            element={
+              <RequireAuth GD={false} FM={false} noAdmin={true}>
+                <MyBookingsPage />
+              </RequireAuth>
+            }
+            errorElement={<RouteError />}
+          />
+        </Route>
 
-				<Route path="admin">
-					<Route
-						path="bookings"
-						element={
-							<RequireAuth GD={false} FM={false} Admin={true}>
-								<AdminBookingsPage />
-							</RequireAuth>
-						}
-						errorElement={<RouteError />}
-					/>
-					<Route
-						path="facilities"
-						element={
-							<RequireAuth GD={false} FM={false} Admin={true}>
-								<AdminFacilitiesPage />
-							</RequireAuth>
-						}
-						errorElement={<RouteError />}
-					/>
-				</Route>
-			</Route>
-			<Route path="*" element={<PageNotFound />} />
-		</>
-	)
+        <Route path="admin">
+          <Route
+            path="bookings"
+            element={
+              <RequireAuth GD={false} FM={false} Admin={true}>
+                <AdminBookingsPage />
+              </RequireAuth>
+            }
+            errorElement={<RouteError />}
+          />
+          <Route
+            path="facilities"
+            element={
+              <RequireAuth GD={false} FM={false} Admin={true}>
+                <AdminFacilitiesPage />
+              </RequireAuth>
+            }
+            errorElement={<RouteError />}
+          />
+        </Route>
+      </Route>
+      <Route path="*" element={<PageNotFound />} />
+    </>
+  )
 );
 
 const queryClient = new QueryClient();
 function App() {
-	return (
-		<QueryClientProvider client={queryClient}>
-			<AuthProvider>
-				<RouterProvider router={router} />
-			</AuthProvider>
-		</QueryClientProvider>
-	);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
