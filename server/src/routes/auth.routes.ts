3a6381a8b9448355ee87761b51da91f97da81725
Shivam77607 <shivamsharma77607@gmail.com>
2023-12-06 19:27:01 +0530

    Add register route to authRouter

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { authLogin, authLogout } from "../controllers/auth.controllers";
+import {
+  authLogin,
+  authLogout,
+  authRegister,
+} from "../controllers/auth.controllers";
 
 const authRouter = express.Router();
 
 authRouter.post("/login", authLogin);
 authRouter.post("/logout", authLogout);
+authRouter.post("/register", authRegister);
 
 export default authRouter;
