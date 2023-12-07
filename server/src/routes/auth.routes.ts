3a060b666be0cd447dfc894bbd7a8e904b6144a6
Vector-ops <sumith2827@gmail.com>
2023-12-07 19:57:13 +0530

    Add change password functionality to auth routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import { authLogin, authLogout } from "../controllers/auth.controllers";
+import {
+	authLogin,
+	authLogout,
+	changePassword,
+} from "../controllers/auth.controllers";
+import validateSession from "../middleware/validateSession";
 
 const authRouter = express.Router();
 
 authRouter.post("/login", authLogin);
 authRouter.post("/logout", authLogout);
+authRouter.post("/change-password", validateSession, changePassword);
 
 export default authRouter;
