3ba835b530175b7ee5116f1d4756499b7c817af3
Shivam77607 <shivamsharma77607@gmail.com>
2024-04-24 20:50:53 +0530

    Update CORS configuration in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,57 +18,49 @@ import facilityRouter from "./routes/facility.routes";
 import logger from "./utils/logger";
 
 const PORT = process.env.PORT || 3000;
-// const corsOptions = {
-//   origin: [
-//     "http://localhost:5173",
-//     process.env.CLIENT_URL,
-//     "https://facility-bookings-manager.vercel.app",
-//     "http://localhost:5000",
-//     "http://localhost:8000",
-//   ],
-//   credentials: true,
-//   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-// };
+const corsOptions = {
+  origin:
+    "http://localhost:5173" ||
+    process.env.CLIENT_URL ||
+    "http://localhost:5000" ||
+    "http://localhost:8000",
+  methods: ["GET", "OPTIONS", "PATCH", "DELETE", "POST", "PUT"],
+  allowedHeaders: [
+    "X-CSRF-Token",
+    "X-Requested-With",
+    "Accept",
+    "Accept-Version",
+    "Content-Length",
+    "Content-MD5",
+    "Content-Type",
+    "Date",
+    "X-Api-Version",
+  ],
+  credentials: true,
+};
 
 const app: Express = express();
 app.use(cors());
 
-app.use(function (req, res, next) {
-	res.setHeader("Access-Control-Allow-Credentials", "true");
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"GET,OPTIONS,PATCH,DELETE,POST,PUT"
-	);
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-	);
-	if (req.method === "OPTIONS") {
-		res.sendStatus(200);
-	} else {
-		next();
-	}
-});
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(
-	expressSession({
-		name: "sid",
-		cookie: {
-			maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
-		},
-		secret: process.env.SESSION_SECRET!,
-		resave: true,
-		saveUninitialized: true,
-		// @ts-ignore
-		store: new PrismaSessionStore(prisma, {
-			checkPeriod: 2 * 60 * 1000, // 2 mins
-			dbRecordIdIsSessionId: true,
-			dbRecordIdFunction: undefined,
-		}),
-	})
+  expressSession({
+    name: "sid",
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
+    },
+    secret: process.env.SESSION_SECRET!,
+    resave: true,
+    saveUninitialized: true,
+    // @ts-ignore
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, // 2 mins
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
 );
 // app.use(function (request, response, next) {
 //   response.header("Access-Control-Allow-Origin", "*");
@@ -89,23 +81,23 @@ app.use(notFound);
 app.use(errorHandler);
 
 const startServer = () => {
-	app.listen(PORT, () => {
-		console.log(`Server listening at http://localhost:${PORT}`);
-		logger.info(
-			`Starting Server on port ${PORT}, Server URL: http://localhost:${PORT}, database: ${process.env.DATABASE}, database URL: ${process.env.DATABASE_URL}`
-		);
-	});
+  app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+    logger.info(
+      `Starting Server on port ${PORT}, Server URL: http://localhost:${PORT}, database: ${process.env.DATABASE}, database URL: ${process.env.DATABASE_URL}`
+    );
+  });
 };
 
 process.on("unhandledRejection", (err: any) => {
-	console.error(err);
-	logger.error(err.message);
-	process.exit(1);
+  console.error(err);
+  logger.error(err.message);
+  process.exit(1);
 });
 
 process.on("SIGINT", () => {
-	logger.info("SIGINT received, shutting down gracefully");
-	process.exit(0);
+  logger.info("SIGINT received, shutting down gracefully");
+  process.exit(0);
 });
 
 startServer();
