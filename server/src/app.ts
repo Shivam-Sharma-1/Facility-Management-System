ee3e9af4669311212a66062421ddb3fd37e0df95
Vector-ops <sumith2827@gmail.com>
2024-04-24 12:29:39 +0530

    refactor cors middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -35,13 +35,6 @@ const app: Express = express();
 app.use(cors());
 
 app.use(function (req, res, next) {
-	// res.header("Access-Control-Allow-Origin", "*");
-	// res.header(
-	// 	"Access-Control-Allow-Headers",
-	// 	"Origin, X-Requested-With, Content-Type, Accept"
-	// );
-	// res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-	// res.header("Access-Control-Allow-Credentials", "true");
 	res.setHeader("Access-Control-Allow-Credentials", "true");
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader(
@@ -52,7 +45,11 @@ app.use(function (req, res, next) {
 		"Access-Control-Allow-Headers",
 		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
 	);
-	next();
+	if (req.method === "OPTIONS") {
+		res.sendStatus(200);
+	} else {
+		next();
+	}
 });
 
 app.use(express.json());
