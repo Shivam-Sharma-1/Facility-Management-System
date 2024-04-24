80b82b2f1b573bb901cda57ea564ea58b8f2956e
Vector-ops <sumith2827@gmail.com>
2024-04-24 22:19:44 +0530

    refactor code for https

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,20 +19,20 @@ import logger from "./utils/logger";
 
 const PORT = process.env.PORT || 3000;
 export const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  methods: ["GET", "OPTIONS", "PATCH", "DELETE", "POST", "PUT"],
-  allowedHeaders: [
-    "X-CSRF-Token",
-    "X-Requested-With",
-    "Accept",
-    "Accept-Version",
-    "Content-Length",
-    "Content-MD5",
-    "Content-Type",
-    "Date",
-    "X-Api-Version",
-  ],
-  credentials: true,
+	origin: process.env.CLIENT_URL,
+	methods: ["GET", "OPTIONS", "PATCH", "DELETE", "POST", "PUT"],
+	allowedHeaders: [
+		"X-CSRF-Token",
+		"X-Requested-With",
+		"Accept",
+		"Accept-Version",
+		"Content-Length",
+		"Content-MD5",
+		"Content-Type",
+		"Date",
+		"X-Api-Version",
+	],
+	credentials: true,
 };
 
 const app: Express = express();
@@ -41,21 +41,23 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(
-  expressSession({
-    name: "sid",
-    cookie: {
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
-    },
-    secret: process.env.SESSION_SECRET!,
-    resave: true,
-    saveUninitialized: true,
-    // @ts-ignore
-    store: new PrismaSessionStore(prisma, {
-      checkPeriod: 2 * 60 * 1000, // 2 mins
-      dbRecordIdIsSessionId: true,
-      dbRecordIdFunction: undefined,
-    }),
-  })
+	expressSession({
+		name: "sid",
+		cookie: {
+			maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
+			httpOnly: false,
+			secure: true,
+		},
+		secret: process.env.SESSION_SECRET!,
+		resave: true,
+		saveUninitialized: true,
+		// @ts-ignore
+		store: new PrismaSessionStore(prisma, {
+			checkPeriod: 2 * 60 * 1000, // 2 mins
+			dbRecordIdIsSessionId: true,
+			dbRecordIdFunction: undefined,
+		}),
+	})
 );
 
 app.use("/auth", authRouter);
@@ -68,23 +70,23 @@ app.use(notFound);
 app.use(errorHandler);
 
 const startServer = () => {
-  app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
-    logger.info(
-      `Starting Server on port ${PORT}, Server URL: http://localhost:${PORT}, database: ${process.env.DATABASE}, database URL: ${process.env.DATABASE_URL}`
-    );
-  });
+	app.listen(PORT, () => {
+		console.log(`Server listening at http://localhost:${PORT}`);
+		logger.info(
+			`Starting Server on port ${PORT}, Server URL: http://localhost:${PORT}, database: ${process.env.DATABASE}, database URL: ${process.env.DATABASE_URL}`
+		);
+	});
 };
 
 process.on("unhandledRejection", (err: any) => {
-  console.error(err);
-  logger.error(err.message);
-  process.exit(1);
+	console.error(err);
+	logger.error(err.message);
+	process.exit(1);
 });
 
 process.on("SIGINT", () => {
-  logger.info("SIGINT received, shutting down gracefully");
-  process.exit(0);
+	logger.info("SIGINT received, shutting down gracefully");
+	process.exit(0);
 });
 
 startServer();
